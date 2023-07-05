f208b17ba6782c679984c15f55ff86927b1999c9
BeiyaC <anis.grigah@le-campus-numerique.fr>
2023-07-05 13:41:31 +0200

    Initiate project

---
+++ src/main/java/com/app/javaddmain/JavaDdMainApplication.java
@@ -0,0 +1,13 @@
+package com.app.javaddmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDdMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDdMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/javaddmain/JavaDdMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.javaddmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDdMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
